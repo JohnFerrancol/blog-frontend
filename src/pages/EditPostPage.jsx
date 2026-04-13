b9d1b872b9a191f642a099ecb53cda2ace9e00e2
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2026-04-13 19:22:25 +0800

    fix: handled UX where posts that does belong to the admin can access the edit form

--- src/pages/EditPostPage.jsx
+++ src/pages/EditPostPage.jsx
@@ -1,23 +1,32 @@
 import { useParams } from 'react-router';
 import { useContext, useState, useEffect } from 'react';
+import AuthContext from '../context/AuthContext';
 import BlogContext from '../context/BlogContext';
 import PostForm from '../components/PostForm';
+import { useNavigate } from 'react-router';
 
 const EditPostPage = () => {
   const { id } = useParams();
+  const { user, loading } = useContext(AuthContext);
   const { getPostDetails } = useContext(BlogContext);
 
   const [post, setPost] = useState(null);
 
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchPost = async () => {
       const data = await getPostDetails(id);
+
+      if (data.post.authorId !== user.id) {
+        navigate('/');
+        return;
+      }
       setPost(data.post);
     };
     fetchPost();
   }, [id]);
 
-  if (!post) return <div>Loading...</div>;
+  if (!post || loading) return <div>Loading...</div>;
 
   return (
     <div className="flex justify-center mt-20">
