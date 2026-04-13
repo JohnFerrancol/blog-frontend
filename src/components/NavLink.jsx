ad1517487a46426e00356d099182f813b6eefa08
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2026-04-13 21:07:30 +0800

    refactor: remove redudant comments

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,9 +1,7 @@
-// NavLink.js
 import { Link } from 'react-router';
 
 const NavLink = ({ route, content, className = '' }) => {
   return (
-    // We move the styling responsibility to where the component is used
     <Link to={route} className={`no-underline ${className} font-semibold`}>
       {content}
     </Link>
